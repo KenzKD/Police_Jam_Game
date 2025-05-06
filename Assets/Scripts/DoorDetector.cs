ac1d6c06f7e5fe560e2d6b413691b84bfcd4586c
bobettes1 <kenzelkdsouza@gmail.com>
2025-05-06 17:01:46 +0400

    Got Door to Open

---
+++ Assets/Scripts/DoorDetector.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DoorDetector : MonoBehaviour
+{
+    public GameObject Door;
+    private static readonly int isDetected = Animator.StringToHash("isDetected");
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Player Detected");
+        Door.GetComponent<Animator>().SetBool(isDetected, true);
+    }
+}
